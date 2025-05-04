a49bd06a3dabad1dfd88949a7798f8321d8d13ad
carlosgrprr <carlosgrprr@gmail.com>
2025-05-04 23:22:57 +0200

    Commit de desarrollo 3

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,7 @@ const swaggerJsDoc = require('swagger-jsdoc');
 const userRoutes = require('./routes/userRoutes');
 const blogRoutes = require('./routes/blogRoutes');
 const marketplaceRoutes = require('./routes/marketplaceRoutes');
-<<<<<<< HEAD
-=======
 require('dotenv').config(); // Load environment variables
->>>>>>> 48b70d8 (Commit inicial de prueba)
 
 const PORT = 3000;
 const isTestMode = process.env.NODE_ENV === 'test'; // Check if the application is running in test mode
@@ -19,23 +16,16 @@ function createServer() {
   const app = express();
 
   // MongoDB connection
-<<<<<<< HEAD
-  mongoose.connect('mongodb://localhost:27017/fruddi', {
-=======
   mongoose.connect(process.env.MONGO_URI, {
->>>>>>> 48b70d8 (Commit inicial de prueba)
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-<<<<<<< HEAD
-=======
   // Replace hardcoded JWT secret (if used elsewhere in the code)
   const jwtSecret = process.env.JWT_SECRET;
 
->>>>>>> 48b70d8 (Commit inicial de prueba)
   // Swagger configuration
   const swaggerOptions = {
     swaggerDefinition: {
